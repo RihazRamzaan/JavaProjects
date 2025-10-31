c7523ab4e18b94d529ebf2bf5a5e3a19d7107ae9
RihazRamzaan <rihazramzaan@gmail.com>
2025-10-31 10:52:29 +0530

    commit temperature converter

---
+++ src/TemperatureCoverter.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class TemperatureCoverter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.print(" Enter the temperature: ");
+        temp = sc.nextDouble();
+
+        System.out.print(" Convert to Celsius or Fahrenheit (C or F): ");
+        unit = sc.next().toUpperCase();
+
+        newTemp = (unit.equals("C") ? (temp - 32) * 5 / 9 : (temp * 5 / 9) + 32);
+
+        System.out.printf("The converted temperature: %.1f°%s",newTemp, unit);
+
+
+        sc.close();
+    }
+}
